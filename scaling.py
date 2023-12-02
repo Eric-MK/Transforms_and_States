93f46f0eb1e09a6f872cbad97ed006d6f58a59af
unknown <mutungaeric839@gmail.com>
2023-12-02 16:43:03 +0300

    scaling

--- scaling.py
+++ scaling.py
@@ -7,22 +7,25 @@ ctx = cairo.Context(surface)
 ctx.set_source_rgb(0.8,0.8,0.8)
 ctx.paint()
 
-# rectangle blue
-ctx.set_source_rgb(0,0,0.5)
-ctx.translate(200, 300)
-ctx.rectangle(-100, -100, 200, 200)
+
+width = 100
+height = 100
+
+# red rectangle
+ctx.set_source_rgb(1,0,0)
+ctx.rectangle(150, 200, width, height)
 ctx.fill()
 
-# rectangle green
-ctx.set_source_rgb(0,0.5,0.5)
-ctx.translate(200, 100)
-ctx.rectangle(-100, -100, 200, 200)
+# rectangle blue
+ctx.scale(2/3,2/3)
+ctx.set_source_rgb(0,0,1)
+ctx.rectangle(300, 150, width, height)
 ctx.fill()
 
 # Save the result to a PNG file in the "output" folder
 output_folder = "output_folder"
 
-output_path = os.path.join(output_folder, "translation.png")
+output_path = os.path.join(output_folder, "scaling.png")
 surface.write_to_png(output_path)
 
 print(f"Saved the result to: {output_path}")
